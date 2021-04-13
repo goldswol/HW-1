e86b682b70f18394e92903cf962ffea03f2e36c7
Luke Goldsworthy <goldswol@oregonstate.edu>
2021-04-12 18:16:52 -0700

    Still broked, hopefully no segfault?

--- mergesort.cpp
+++ mergesort.cpp
@@ -80,7 +80,7 @@ void mergeArrays(int arr[], int pos1, int pos2, int pos3) {
       k++;
     }
 
-    while (i < n1) {
+    /*while (i < n1) {
       arr[k]=L[i];
       i++;
       k++;
@@ -89,7 +89,7 @@ void mergeArrays(int arr[], int pos1, int pos2, int pos3) {
       arr[k]=R[j];
       j++;
       k++;
-    }
+    }*/
 }
 
 void mergeSort(int arr[],int pos1,int pos3){
